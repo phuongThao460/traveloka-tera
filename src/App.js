4a8c632cfa261b415a5de7a0fa8afea66a0d6a7e
phuongThao460 <blackcompany1998@gmail.com>
2021-06-10 22:09:58 +0700

    Add form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css'
 import LoginForm from './components/pages/LoginForm'
 import Home from './components/pages/Home'
+import RegisterForm from './components/pages/RegisterForm'
 //import Services from './components/pages/Services';
 import Products from './components/pages/Products'
 import RegistrationDetail from './components/pages/RegistrationDetail'
 import AddHomeBlock from './components/pages/AddHomeBlock'
 import ListHome from './components/pages/ListHome'
 import ListApartment from './components/pages/ListApartment'
-//import PropertyFacilities from './components/pages/PropertyFacilities'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
         <Route path='/' exact component={LoginForm} />
         <Switch>
           <Route path='/home' component={Home} />
+          <Route path='/register' component={RegisterForm} />
           <Route path='/products/:id' component={Products} />
           <Route path='/registrationDetail' component={RegistrationDetail}/>
           <Route path='/AddHomeBlock/:idTk' component={AddHomeBlock}/>
